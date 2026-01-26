fe2d966b10a28c1a657a9c76cb6ab2dd4ab3c4ae
cliff.yang <cliff.yang@okg.com>
2026-01-26 12:24:29 +0800

    add pistache  build script

---
+++ build_pistache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PISTACHE_DIR="${SCRIPT_DIR}/depends/pistache"
+BUILD_DIR="${PISTACHE_DIR}/build"
+
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+
+cmake -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPISTACHE_BUILD_TESTS=OFF \
+    -DLIB_EVENT_INCLUDE_DIR=/opt/homebrew/opt/libevent/include \
+    ..
+
+make -j$(sysctl -n hw.ncpu)
